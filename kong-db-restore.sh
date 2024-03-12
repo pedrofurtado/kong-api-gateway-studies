382cd66670ee42cbba4e8fe93a11e8b5b485422e
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-03-12 07:10:58 -0300

    restart kong and db after restore

--- kong-db-restore.sh
+++ kong-db-restore.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cat backup-kong-db.sql | docker exec -i kong-api-gateway-studies_kong_database_pg_1 psql -U kongdbuser -d kongdb
+docker container restart kong-api-gateway-studies_kong_database_pg_1
+docker container restart kong-api-gateway-studies_kong_1
